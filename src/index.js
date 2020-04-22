211eb6c2c3edb152916c8ae6c8bf2536d49b473c
小黑 <getaikejiff@getaikejideMac-mini-8.local>
2020-04-22 15:57:51 +0800

    feat(增加基础路由页面)

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import LargeIndex from './views/LargeIndex'
+
+ReactDOM.render(React.createElement(LargeIndex), document.getElementById('app'))
